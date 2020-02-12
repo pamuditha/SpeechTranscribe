0bb7b039a799848378da5ac7a99d050de54c8331
Imalka De Silva <pamuditha.imalka@gmail.com>
2020-02-12 10:20:15 +0530

    first commit

---
+++ src/test/java/org/codelanka/speech/SpeechTranscribe/SpeechTranscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codelanka.speech.SpeechTranscribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechTranscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
